8896d365f8f0cf48948caecd80a51b725294819d
Lawrence <lawrence@thejs.ninja>
2017-02-25 18:05:01 +0000

    Fix for Named Testing

--- examples/example_domains/start.js
+++ examples/example_domains/start.js
@@ -2,15 +2,14 @@ const syrup = require('../../');
 
 syrup.config(`./config.yaml`);
 
-syrup.scenario('example.org1', `${__dirname}/test-example.org`, [], 'IEBrowser');
-syrup.scenario('example.org2', `${__dirname}/test-example.org`, [], 'IEBrowser');
-syrup.scenario('example.org3', `${__dirname}/test-example.org`, [], 'IEBrowser');
-syrup.scenario('example.org4', `${__dirname}/test-example.org`, [], 'IEBrowser');
-syrup.scenario('example.org5', `${__dirname}/test-example.org`, [], 'ChromeBrowser');
-syrup.scenario('example.org6', `${__dirname}/test-example.org`, [], 'ChromeBrowser');
-syrup.scenario('example.org7', `${__dirname}/test-example.org`, [], 'ChromeBrowser');
-syrup.scenario('example.org8', `${__dirname}/test-example.org`, [], 'ChromeBrowser');
-syrup.scenario('example.org9', `${__dirname}/test-example.org`, [], 'ChromeBrowser');
+syrup.scenario('example.org - 1', `${__dirname}/test-example.org`, [], 'IEBrowser');
+syrup.scenario('example.org - 2', `${__dirname}/test-example.org`, [], 'IEBrowser');
+syrup.scenario('example.org - 3', `${__dirname}/test-example.org`, [], 'IEBrowser');
+syrup.scenario('example.org - 4', `${__dirname}/test-example.org`, [], 'IEBrowser');
+syrup.scenario('example.org - 5', `${__dirname}/test-example.org`, [], 'ChromeBrowser');
+syrup.scenario('example.org - 6', `${__dirname}/test-example.org`, [], 'ChromeBrowser');
+syrup.scenario('example.org - 7', `${__dirname}/test-example.org`, [], 'ChromeBrowser');
+syrup.scenario('example.org - 8', `${__dirname}/test-example.org`, [], 'ChromeBrowser');
 
 syrup.pour(function (error, results) {
     console.log(JSON.stringify(results));
