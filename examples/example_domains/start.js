5fcd40241a92865dbeb65dd583ed25415d418bab
Lawrence <lawrence@thejs.ninja>
2017-02-25 23:34:11 +0000

    Adds some intra test dependancies

--- examples/example_domains/start.js
+++ examples/example_domains/start.js
@@ -4,12 +4,20 @@ syrup.config(`./config.yaml`);
 
 syrup.scenario('example.org - 1', `${__dirname}/test-example.org`, [], 'IEBrowser');
 syrup.scenario('example.org - 2', `${__dirname}/test-example.org`, [], 'IEBrowser');
-syrup.scenario('example.org - 3', `${__dirname}/test-example.org`, [], 'IEBrowser');
+syrup.scenario('example.org - 3', `${__dirname}/test-example.org`, ['example.org - 2'], 'IEBrowser');
 syrup.scenario('example.org - 4', `${__dirname}/test-example.org`, [], 'IEBrowser');
-syrup.scenario('example.org - 5', `${__dirname}/test-example.org`, [], 'ChromeBrowser');
-syrup.scenario('example.org - 6', `${__dirname}/test-example.org`, [], 'ChromeBrowser');
-syrup.scenario('example.org - 7', `${__dirname}/test-example.org`, [], 'ChromeBrowser');
+syrup.scenario('example.org - 5', `${__dirname}/test-example.org`, ['example.org - 4'], 'ChromeBrowser');
+syrup.scenario('example.org - 6', `${__dirname}/test-example.org`, ['example.org - 1'], 'ChromeBrowser');
+syrup.scenario('example.org - 7', `${__dirname}/test-example.org`, ['example.org - 6'], 'ChromeBrowser');
 syrup.scenario('example.org - 8', `${__dirname}/test-example.org`, [], 'ChromeBrowser');
+syrup.scenario('example.org - 9', `${__dirname}/test-example.org`, [], 'IEBrowser');
+syrup.scenario('example.org - 10', `${__dirname}/test-example.org`, ['example.org - 8'], 'IEBrowser');
+syrup.scenario('example.org - 11', `${__dirname}/test-example.org`, [], 'IEBrowser');
+syrup.scenario('example.org - 12', `${__dirname}/test-example.org`, [], 'IEBrowser');
+syrup.scenario('example.org - 13', `${__dirname}/test-example.org`, [], 'ChromeBrowser');
+syrup.scenario('example.org - 14', `${__dirname}/test-example.org`, [], 'ChromeBrowser');
+syrup.scenario('example.org - 15', `${__dirname}/test-example.org`, [], 'ChromeBrowser');
+syrup.scenario('example.org - 16', `${__dirname}/test-example.org`, [], 'ChromeBrowser');
 
 syrup.pour(function (error, results) {
     console.log(JSON.stringify(results));
