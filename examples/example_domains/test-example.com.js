0a4f438889acd52954d536495a828fbbe3222691
Lawrence <lawrence@thejs.ninja>
2017-02-26 22:25:22 +0000

    Fix to example domain snippets

--- examples/example_domains/test-example.com.js
+++ examples/example_domains/test-example.com.js
@@ -1,12 +1,14 @@
 var assert = require('assert');
 describe('Example.com', function() {
     describe('page content', function() {
-        it('should navigate to example.com', () =>
+        it('should navigate to example.com', (done) => {
             Browser.url('http://example.com')
-        );
-        it('should find a heading containing "Example Domain"', () =>
+                .then(done);
+        });
+        it('should find a heading containing "Example Domain"', (done) => {
             Browser.getText('h1')
                 .then((text) => assert(text == 'Example Domain'))
-        );
+                .then(done);
+        });
     });
 });
