a0087b913982256c574873bc2cbf3075036492ea
Lawrence <lawrence@acode.ninja>
2017-03-13 23:07:07 +0000

    Remove Syrup.pour() callbacks

--- examples/console/start.js
+++ examples/console/start.js
@@ -5,16 +5,4 @@ syrup.scenario('object', `${__dirname}/test-object`);
 syrup.scenario('save', `${__dirname}/test-save`);
 syrup.scenario('get', `${__dirname}/test-get`, ['save']);
 
-syrup.pour(function (error, results) {
-    console.log(JSON.stringify(results));
-}, function (error, results) {
-    // Do something with the progress update
-    // Results Example:
-    //
-    //      {
-    //          array: 'done',
-    //          object: 'done',
-    //          save: 'done',
-    //          get: 'pending'
-    //      }
-});
+syrup.pour();
