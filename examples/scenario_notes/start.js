74748d535afb199abf0a771a6830e02fc53b5c72
Lawrence <lawrence@acode.ninja>
2017-03-14 16:19:00 +0000

    Adds example on scenario notes (descriptive notes added to scenarios on test)

--- examples/scenario_notes/start.js
+++ examples/scenario_notes/start.js
@@ -6,4 +6,9 @@ syrup.enableDebug()
         notes: 'Test that standard array methods work',
         entrypoint: `${__dirname}/test-array`,
     })
+    .scenario({
+        name: 'object',
+        notes: 'Test that standard object methods work',
+        entrypoint: `${__dirname}/test-object`,
+    })
     .pour();
