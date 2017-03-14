e35ab29ca8af250eb004efb4ffbc24c62e2950ce
Lawrence <lawrence@acode.ninja>
2017-03-14 20:49:44 +0000

    PhantomJsBrowserWorker starts it's own phatomjs ghost service to run tests on

--- src/Workers/PhantomJsBrowserWorker.js
+++ src/Workers/PhantomJsBrowserWorker.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const child_process = require('child_process');
+
 const BrowserWorker = require('./BrowserWorker');
 const WebDriver = require('webdriverio');
 
@@ -8,19 +10,69 @@ class PhantomJsBrowserWorker extends BrowserWorker {
         super(scenario);
     }
     setup(done) {
-        if(typeof Browser === 'undefined') {
-            global.Browser = WebDriver.remote({
-                capabilities: { browserName: 'phantomjs' },
-                services: ['phantomjs']
-            });
-        }
+        var phantomJsPort = Math.floor(Math.random() * (7000 - 6000 + 1)) + 6000;
 
-        super.setup(() => {
-            Browser.init().then(() => done() , done);
-        })
+        this.phantomService = child_process.spawn(
+            'phantomjs',
+            ['--webdriver', `localhost:${phantomJsPort}`],
+            {
+                cwd: `${__dirname}/../../node_modules/.bin/`
+            }
+        );
+
+        this.phantomService.stdout.on('data', (data) => {
+            let failedToStart = `${data}`.match(/\[ERROR[\s\S]+(main.fail)/);
+            let upAndRunning = `${data}`.match(/running on port ([\d]+)/);
+
+            if(failedToStart) {
+                process.send({ log: `PhantomJs failed to start on :${phantomJsPort}` });
+                process.send({ exit: true });
+            }
+
+            if(upAndRunning) {
+                process.send({ log: `PhantomJs started on :${phantomJsPort}` });
+
+                if(typeof global.Browser === 'undefined') {
+                    global.Browser = WebDriver.remote({
+                        capabilities: { browserName: 'phantomjs' },
+                        services: ['phantomjs'],
+                        host: `127.0.0.1`,
+                        port: `${phantomJsPort}`
+                    });
+                }
+
+                super.setup(() => {
+                    Browser.init().then(() => {
+                        process.send({ log: `Browser started on :${phantomJsPort}` });
+                        done();
+                    }, () => {
+                        process.send({ log: `Browser failed to start on :${phantomJsPort}` });
+                        process.send({ exit: true });
+                    });
+                });
+            }
+        });
+
+        this.phantomService.stderr.on('data', (data) => {
+          process.send({ log: `PhantomJs threw an error: ${data}` });
+        });
+
+        this.phantomService.on('error', (code) => {
+          process.send({ log: `PhantomJs exited with error ${code}` });
+        });
+
+        this.phantomService.on('close', (code) => {
+          process.send({ log: `PhantomJs exited with code ${code}` });
+        });
     }
     teardown(done) {
-        super.teardown(() => Browser.end().then(() => done() , done));
+        super.teardown(() => {
+            Browser.end()
+                .then(() => {
+                    this.phantomService.kill();
+                    done();
+                }, done)
+        });
     }
 }
 
