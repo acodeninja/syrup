b3bf07a703cb0de729c34bd98bf2d0d34d800544
Lawrence <lawrence@acode.ninja>
2017-07-27 11:08:54 +0100

    Add done method to the base worker to call down methods on globals

--- src/workers/BaseWorker.js
+++ src/workers/BaseWorker.js
@@ -66,6 +66,26 @@ class BaseWorker {
         EventsBus.emit('worker:started', { name: this.scenario.name });
         done();
     }
+    done(done) {
+        try {
+            async.filter(this._globals, function(requirement, callback) {
+                let theGlobal = `${__dirname}/../globals/` + (typeof requirement === 'string' ? requirement : requirement.name);
+                let options = typeof requirement === 'object' ? requirement.options : {};
+
+                requirement = require(theGlobal);
+
+                if (typeof requirement === 'object' && _.has(requirement, 'down')) {
+                    requirement.down(callback, options);
+                } else {
+                    callback();
+                }
+            }, function (err) {
+                done(err);
+            });
+        } catch (err) {
+            throw new RequirementDoesNotExistError(err.toString().replace(/^([\s\S]+(globals\/))/, '').replace("\'", ''));
+        }
+    }
 }
 
 module.exports = BaseWorker;
