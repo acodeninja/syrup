639232f0d4bd80e021949e4ead8d2b7da755d503
Lawrence <lawrence@acode.ninja>
2017-11-30 14:09:30 +0000

    Create connection to browserstack-local if the browserstack.local key is set in config

--- src/globals/browser.js
+++ src/globals/browser.js
@@ -1,12 +1,31 @@
 'use strict';
 
-const WebDriver = require('webdriverio');
 const _ = require('lodash');
-
+const WebDriver = require('webdriverio');
+const BrowsertStack = require('browserstack-local');
+const BrowsertStackLocal = new BrowsertStack.Local();
 const EventsBus = require('../libs/EventsBus');
 
 module.exports = {
     up: (done, options) => {
+        if (
+              options.desiredCapabilities['browserstack.local']
+        ) {
+            new Promise(function(resolve, reject) {
+                BrowsertStackLocal.start({
+                    'key': options.key
+                }, function(error) {
+                    if (error) return reject(error);
+                    resolve();
+                });
+            }).then(() => {
+                module.exports.connect(done, options);
+            });
+        } else {
+            module.exports.connect(done, options);
+        }
+    },
+    connect: (done, options) => {
         global.Browser = WebDriver.remote(_.extend({}, options));
 
         Browser.init().then(() => {
@@ -18,6 +37,6 @@ module.exports = {
     },
     down: (done, options) => {
         Browser.end().then(() => {done()}, () => {done();});
+        BrowsertStackLocal.stop(function() {});
     }
 };
-
